de4df4a8403e141a28bec6be8088506754282591
Kimberley <111291744+Kimberleyvm@users.noreply.github.com>
2023-10-21 20:07:11 +0200

    Update script.js
    added the read more for the about section

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// JavaScript
+// cascading menu
 
 function toggleMenu() {
     var menu = document.getElementById("menu");
@@ -6,5 +6,17 @@ function toggleMenu() {
         menu.style.display = "none";
     } else {
         menu.style.display = "block";
+    }
+}
+// read-more
+function toggleAbout() {
+    var content = document.getElementById("about-content");
+    var btn = document.getElementById("toggle-btn");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+        btn.innerHTML = "Lees minder";
+    } else {
+        content.style.display = "none";
+        btn.innerHTML = "Lees meer";
     }
 }
